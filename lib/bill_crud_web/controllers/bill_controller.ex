8f7889b6bdba2bffd4a30c4cabc52b580dc46208
Cassio Godinho <cassiopgodinho@gmail.com>
2022-07-10 21:40:42 -0300

    wip

--- lib/bill_crud_web/controllers/bill_controller.ex
+++ lib/bill_crud_web/controllers/bill_controller.ex
@@ -6,7 +6,8 @@ defmodule BillCrudWeb.BillController do
 
   def index(conn, _params) do
     bills = Pages.list_bills()
-    render(conn, :index, bills: bills)
+    changeset = Pages.change_bill(%Bill{})
+    render(conn, :index, bills: bills, changeset: changeset)
   end
 
   def new(conn, _params) do
@@ -17,9 +18,14 @@ defmodule BillCrudWeb.BillController do
   def create(conn, %{"bill" => bill_params}) do
     case Pages.create_bill(bill_params) do
       {:ok, bill} ->
-        conn
-        |> put_flash(:info, "Bill created successfully.")
-        |> redirect(to: Routes.bill_path(conn, :show, bill))
+        if PhoenixTurbo.ControllerHelper.turbo_stream_request?(conn) do
+          conn
+          |> PhoenixTurbo.ControllerHelper.render_turbo_stream(:show, bill: bill)
+        else
+          conn
+          |> put_flash(:info, "Bill created successfully.")
+          |> redirect(to: Routes.bill_path(conn, :show, bill))
+        end
 
       {:error, %Ecto.Changeset{} = changeset} ->
         conn
@@ -30,13 +36,13 @@ defmodule BillCrudWeb.BillController do
 
   def show(conn, %{"id" => id}) do
     bill = Pages.get_bill!(id)
-    render(conn, "show.html", bill: bill)
+    render(conn, :show, bill: bill)
   end
 
   def edit(conn, %{"id" => id}) do
     bill = Pages.get_bill!(id)
     changeset = Pages.change_bill(bill)
-    render(conn, "edit.html", bill: bill, changeset: changeset)
+    render(conn, :edit, bill: bill, changeset: changeset)
   end
 
   def update(conn, %{"id" => id, "bill" => bill_params}) do
