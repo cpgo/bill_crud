b495cfd1513045cdebb1aa1994086cc12e8c3554
Cassio Godinho <cassiopgodinho@gmail.com>
2022-08-24 10:02:56 -0300

    swap to dep dev branch

--- lib/bill_crud_web/router.ex
+++ lib/bill_crud_web/router.ex
@@ -8,7 +8,7 @@ defmodule BillCrudWeb.Router do
     plug :put_root_layout, {BillCrudWeb.LayoutView, :root}
     plug :protect_from_forgery
     plug :put_secure_browser_headers
-    plug :handle_turbo_frame
+    plug PhoenixTurbo.HandleTurboPlug
   end
 
   pipeline :api do
