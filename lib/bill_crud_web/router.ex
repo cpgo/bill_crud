bb4b17cfa59f61ebd4347b5908279547e4e6dc94
Cassio Godinho <cassiopgodinho@gmail.com>
2022-08-24 11:01:57 -0300

    inline plug

--- lib/bill_crud_web/router.ex
+++ lib/bill_crud_web/router.ex
@@ -8,7 +8,8 @@ defmodule BillCrudWeb.Router do
     plug :put_root_layout, {BillCrudWeb.LayoutView, :root}
     plug :protect_from_forgery
     plug :put_secure_browser_headers
-    plug PhoenixTurbo.HandleTurboPlug
+    # plug PhoenixTurbo.HandleTurboPlug
+    plug :handle_turbo_frame
   end
 
   pipeline :api do
