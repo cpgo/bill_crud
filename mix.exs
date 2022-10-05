4a9b9b5db8521bd665006bde87a61dd14526a703
Cassio Godinho <cassiopgodinho@gmail.com>
2022-10-05 11:27:59 -0300

    testing appsignal setup

--- mix.exs
+++ mix.exs
@@ -52,7 +52,8 @@ defmodule BillCrud.MixProject do
       {:phoenix_turbo, git: "https://github.com/cpgo/phoenix_turbo.git", branch: "dev"},
       {:tailwind, "~> 0.1", runtime: Mix.env() == :dev},
       {:money, "~> 1.11"},
-      {:ecto_psql_extras, "~> 0.6"}
+      {:ecto_psql_extras, "~> 0.6"},
+      {:appsignal_phoenix, "~> 2.0"}
     ]
   end
 
